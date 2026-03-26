7fe65be0a0d5868732c3aefaa01f667d29d74eb9
Gustavo <gustavomcurado@gmail.com>
2026-03-26 10:12:43 +0000

    Last Week

--- project_1/project_1.py
+++ project_1/project_1.py
@@ -1,4 +1,49 @@
-import numpy
+#### PROJECT 1 
+#### 
+#### Impact of EVs on SS transformer and estimation of its loss of life
+
+
+#### Explicar o workflow do python para este mini projeto
+#### Mostrar a imagem das diferentes partes IRL, da SS e assim
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+##import as pode ser para a bilbioteca a dar plot
+
+
+
+
+#Fazer rede neuronal para o laboratorio 4 de ADRI
+
+
+
+
+
+
+
+
+
+
+
+plt.plot()
+plt.title("")
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
